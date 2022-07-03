876cb69cb76b72548ab19dc979b415f4be62e155
Amrit Bhogal <ambhogal01@gmail.com>
2022-07-03 16:45:10 -0700

    Added computers

---
+++ computer/0/test.lua
@@ -0,0 +1,8 @@
+Farenheit = 10
+Celsius = (Farenheit-32)*5/9
+if (Farenheit == 150)==false then
+    repeat
+        print("farenheit",(Farenheit))
+        Farenheit=Farenheit+10
+    until Farenheit == 160 
+end
